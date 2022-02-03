cfb74cebe97425aa319c3edd2c59c2fb4f4fcd18
pauline-wim <linette.wim@hotmail.com>
2022-02-03 14:37:55 +0100

    Add component Button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from "react"
+import React from "react";
 
 // JS
-import Button from "./components/Button"
+import Button from "./components/Button";
 
 // CSS
-import "./App.css"
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
-class App extends React.Component{
-
-  render(){
-    return()
+import "./App.css";
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+class App extends React.Component {
+  render() {
+    return (
+      <section>
+        <Button />
+        {/* Ajout Pauline */}
+      </section>
+    );
   }
 }
-export default App
+export default App;
